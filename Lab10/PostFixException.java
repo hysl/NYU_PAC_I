75faad9ffbb9521ccf8e61926a3164a8e415b3b7
hysl <47994191+hysl@users.noreply.github.com>
2019-08-24 21:59:16 -0400

    Create PostFixException.java

---
+++ Lab10/PostFixException.java
@@ -0,0 +1,17 @@
+/*
+Helen Li
+December 15, 2017
+*/
+
+public class PostFixException extends RuntimeException
+{
+	public PostFixException()
+	{
+		super();
+	}
+
+	public PostFixException(String message)
+	{
+		super(message);
+	}
+}
