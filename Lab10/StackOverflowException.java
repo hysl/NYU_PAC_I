147a7df4b7f2b8c4f90071eeed4df08dda9c6472
hysl <47994191+hysl@users.noreply.github.com>
2019-08-24 22:00:40 -0400

    Create StackOverflowException.java

---
+++ Lab10/StackOverflowException.java
@@ -0,0 +1,17 @@
+/*
+Helen Li
+December 15, 2017
+*/
+
+public class StackOverflowException extends RuntimeException
+{
+	public StackOverflowException()
+	{
+		super();
+	}
+
+	public StackOverflowException(String message)
+	{
+		super(message);
+	}
+}
