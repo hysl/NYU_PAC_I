ea4f19ed4c2db59a6b7e6289e19e63601def0925
hysl <47994191+hysl@users.noreply.github.com>
2019-08-24 22:01:17 -0400

    Create StackUnderflowException.java

---
+++ Lab10/StackUnderflowException.java
@@ -0,0 +1,17 @@
+/*
+Helen Li
+December 15, 2017
+*/
+
+public class StackUnderflowException extends RuntimeException
+{
+	public StackUnderflowException()
+	{
+		super();
+	}
+
+	public StackUnderflowException(String message)
+	{
+		super(message);
+	}
+}
